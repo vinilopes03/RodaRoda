1be40add0a40b5a838e9f53b0d75160aa9288bcf
vinilopes03 <vinilopes03@gmail.com>
2016-02-16 14:54:18 -0200

    Roda-Roda projeto criado

---
+++ Roda-Roda/src/roda/roda/Player.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package roda.roda;
+
+/**
+ *
+ * @author Vinícius
+ */
+public class Player {
+    
+    private String nome;
+    
+    public Player(){
+        
+    };
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    
+}
